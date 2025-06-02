66a83def696c58cf77f7bcbbdc1866ce6e1cec33
unknown <muktinathrajbanshi786@gmail.com>
2025-06-02 14:28:34 +0545

    Render Multiple Elements inside ReactDOM.render() in ReactJS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-// ReactDOM.render(<h1>Hello World!!</h1>,
-//   document.getElementById("root"));
+ReactDOM.render(
+    // <div>
+    // <h1>Hello World!!</h1>
+    // <p>Please Like and Share </p>
+    // <h2>Please Subscribe my channel</h2>
+    // </div>,
+    [
+    <h1>Hello World!!</h1>,
+    <p>Please Like and Share Too </p>,
+    <h2>Please Subscribe my channel</h2>,
+    ],
+  document.getElementById("root")
+);
 
-var h1 = document.createElement("h1");
-h1.innerHTML = "Muktinath rajbanshi";
-document.getElementById("root").appendChild(h1);
+
+
+
+
