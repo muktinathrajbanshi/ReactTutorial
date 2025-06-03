4ef3c6e0434547b85138bf70c8924310e3fc2d7d
unknown <muktinathrajbanshi786@gmail.com>
2025-06-03 13:10:18 +0545

    ES6 Template Literals in JSX in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const flname = "muktinath rajbanshi";
-
-
+const fname = "muktinath";
+const lname = "rajbanshi";
 
 ReactDOM.render(
 
   <>
-  <h1> my name is {flname} </h1>
+  <h1> {`my name is ${fname} and my last name is ${lname}`}</h1>
   <p> My lucky number is {7+2} </p>
 
   </>,
