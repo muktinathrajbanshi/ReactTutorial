1686e3ecdc3d00edf9d0618c46e30c7f73f15a40
unknown <muktinathrajbanshi786@gmail.com>
2025-06-03 23:31:22 +0545

    Creating Simple Greeting Website using React JS

--- src/index.js
+++ src/index.js
@@ -2,47 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const name = "muktinath";
-const img1 = "https://picsum.photos/200/300";
-const img2 = "https://picsum.photos/220/300";
-const img3 = "https://picsum.photos/230/300";
-const img4 ="https://picsum.photos/240/300";
-const links = "https://muktinathecom.netlify.app/";
 
+let curDate = new Date(2025, 6, 3, 5);
+curDate = curDate.getHours();
+let greeting = "";
 
-// .heading {
-//   font-family: "Josefin Sans", sans-serif;
-//   color: red;
-//   text-align: center;
-//   text-transform: capitalize;
-//   font-weight: bold;
-//   text-shadow: 0px 2px 4px #ffe9c5;
-//   margin: 70px 0;
-// }
+const cssStyle = { };
 
-const heading = {
-  color : "red",
-  textTransform : "capitalize",
-  textAlign : "center",
-  fontWeight: "bold",
-  textShadow: "0px 2px 4px #ffe9c5",
-  margin: "70px 0",
-  fontFamily: '"Josefin Sans", sans-serif'
+// const greet = "Hello Sir,";
 
-};
+if(curDate >=1 && curDate <12) {
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+}else if(curDate>=12 && curDate < 19) {
+  greeting = " Good Afternoon ";
+  cssStyle.color = "orange";
+
+} else {
+  greeting = "Good Night";
+  cssStyle.color = "black";
+
+}
+
 
 ReactDOM.render(
   <>
-  <h1 style={heading}> My name is {name} </h1>
-  <div className="img_div">
-  <img src={img1} alt="lorem images" />
-  <img src={img2} alt="lorem images" />
-  <img src={img3} alt="lorem images" />
-  <a href={links} target="_mukti">
-  <img src={img4} alt="lorem images" />
-  </a>
+  <div>
+  <h1> Hello Sir, <span style={cssStyle}> { greeting } </span> </h1>
   </div>
   </>,
 
- document.getElementById("root")
+document.getElementById("root")
+
 );
