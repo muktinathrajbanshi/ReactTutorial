4569358f7c09f2cee1cf8f578941a0d9b2d7ca9a
unknown <muktinathrajbanshi786@gmail.com>
2025-06-03 12:33:40 +0545

    React Fragment in React JS

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 
 
 ReactDOM.render(
-    // <div>
-    // <h1>Hello World!!</h1>
-    // <p>Please Like and Share </p>
-    // <h2>Please Subscribe my channel</h2>
-    // </div>,
-    [
-    <h1>Hello World!!</h1>,
-    <p>Please Like and Share Too </p>,
-    <h2>Please Subscribe my channel</h2>,
-    ],
+    <>
+    <h1>Hello World!!</h1>
+    <p>Please Like and Share </p>
+    <h2>Please Subscribe my channel</h2>
+    </>,
+    // [
+    // <h1>Hello World!!</h1>,
+    // <p>Please Like and Share Too </p>,
+    // <h2>Please Subscribe my channel</h2>,
+    // ],
   document.getElementById("root")
 );
 
