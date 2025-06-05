68bcb0107582dbf4951cd1dcafeae75863e63749
unknown <muktinathrajbanshi786@gmail.com>
2025-06-05 14:01:09 +0545

    Props in React JS | React JS Project Netflix App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Card from "./Cards";
+import "./index.css";
 
-ReactDOM.render(
-  <App />,
+
+ReactDOM.render( 
+  <>
+    <Card 
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title = "A Netflix Original Series"
+      sname = "DARK"
+      link = "https://www.netflix.com/in/title/80990668?source=35"
+     />
+    <Card 
+      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyv3B-eJLx971WKtQG6mFqEe1f8lx8QCPqfw&s"
+      title = "A Netflix Original Series"
+      sname = "extra curricular"
+      link = "https://www.netflix.com/in/title/80990668?source=35"
+    />
+    <Card
+     imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
+      title = "A Netflix Original Series"
+      sname = "Stranger Things"
+      link = "https://www.netflix.com/in/title/80990668?source=35"
+     />
+  </>,
+   
   document.getElementById("root")
-)
+);
