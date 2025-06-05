c091d4d7f304223f86a9445a1d7a4f2168f6717f
unknown <muktinathrajbanshi786@gmail.com>
2025-06-05 15:29:51 +0545

    just creating one API for invoking data in React

---
+++ src/Sdata.jsx
@@ -0,0 +1,32 @@
+const Sdata = [
+    {
+        sname: "DARK",
+        imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "extra curricular",
+        imgscr: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyv3B-eJLx971WKtQG6mFqEe1f8lx8QCPqfw&s",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+     {
+        sname: "Stranger Things",
+        imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+     {
+        sname: "the vampire Diaries",
+        imgscr: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSNlOADlPPhcsF2Jfqum0pOAfUFuOhb2dAjRhlw58Lx09fQd6QyOArUkhYf9S6Jnb533zpzVO5R4QYM2X0hRS3vkNtzIvYDLX69NuzSMhB_Tg",
+        title: "azon Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+     {
+        sname: "My First-2 Love",
+        imgscr: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+]
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import "./index.css";
 
 ReactDOM.render( 
   <>
+
+    <h1 className="heading_style"> List of top 5 Netflix Series in 2020 </h1>
+
     <Card 
       imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
       title = "A Netflix Original Series"
@@ -22,6 +25,18 @@ ReactDOM.render(
      imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
       title = "A Netflix Original Series"
       sname = "Stranger Things"
+      link = "https://www.netflix.com/in/title/80990668?source=35"
+     />
+      <Card
+      imgsrc="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSNlOADlPPhcsF2Jfqum0pOAfUFuOhb2dAjRhlw58Lx09fQd6QyOArUkhYf9S6Jnb533zpzVO5R4QYM2X0hRS3vkNtzIvYDLX69NuzSMhB_Tg"
+      title = "azon Original Series"
+      sname = "the vampire Diaries"
+      link = "https://www.netflix.com/in/title/80990668?source=35"
+     />
+      <Card
+     imgsrc="https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg"
+      title = "A Netflix Original Series"
+      sname = "My First-2 Love"
       link = "https://www.netflix.com/in/title/80990668?source=35"
      />
   </>,
