813cd9e7de0e2b85ba1d402a19f3c72cfe0ba842
unknown <muktinathrajbanshi786@gmail.com>
2025-06-06 12:47:27 +0545

    If Else Statement in React JS | Conditional Rendering in React JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Card from "./Cards";
 import Sdata from "./Sdata";
+import Netflix from "./Netflix";
+import Amazon from "./Amazon";
 
+const favSeries = "amazon";
+
+const FavS = () => {
+
+
+    if(favSeries === "netflix") {
+       return <Netflix />;
+    }else {
+        return <Amazon />
+    }
+}
 
 const App = () => ( 
   <>
     <h1 className="heading_style"> List of top 6 Netflix Series in 2020 </h1>
-    {Sdata.map((val, index) => {
-      console.log(index);
-      return (
-         <Card 
-          key={val.id}
-          imgsrc={val.imgscr}
-          title = {val.title}
-          sname = {val.sname}
-          link = {val.links}
-        />
-      );
-    })}
+    <FavS />
   </>
    
 );
