04c930c71f66ac9ba602918d7a83f75cbb63ebd8
unknown <muktinathrajbanshi786@gmail.com>
2025-06-08 12:51:49 +0545

    React Login Form Challenge in React

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ const App = () => {
     const [fullName, setFullName] = useState({
         fname: "",
         lname: "",
+        email: "",
+        phone: "",
     });
     
     const inputEvent = (event) => {
         console.log(event.target.value);
         console.log(event.target.name);
 
-        const value = event.target.value;
-        const name = event.target.name;
+        // const value = event.target.value;
+        // const name = event.target.name;
+
+        const {value, name} = event.target;
 
         setFullName((preValue) => {
         // console.log(preValue);
@@ -20,14 +24,33 @@ const App = () => {
            return {
             fname: value,
             lname: preValue.lname,
+            email: preValue.email,
+            phone: preValue.phone,
+
            }; 
         } else if(name === "lName") {
            return {
             fname: preValue.fname,
             lname: value,
+            email: preValue.email,
+            phone: preValue.phone,
+           }; 
+        } else if(name === "email") {
+           return {
+            fname: preValue.fname,
+            lname: preValue.lname,
+            email: value,
+            phone: preValue.phone,
+             }; 
+        }else if(name === "phone") {
+           return {
+            fname: preValue.fname,
+            lname: preValue.lname,
+            email: preValue.email,
+            phone: value,
            }; 
         }
-        })
+        });
 
     };
 
@@ -43,6 +66,8 @@ const App = () => {
             <form onSubmit={onSubmits}>
             <div>
                 <h1> Hello {fullName.fname} {fullName.lname} </h1>
+                <p> {fullName.email} </p>
+                <p> {fullName.phone} </p>
                 <input
                  type="text" 
                  placeholder="Enter Your Name" 
@@ -57,6 +82,21 @@ const App = () => {
                  name="lName"
                  onChange={inputEvent}
                  value={fullName.lname}
+                  />
+                  <input
+                 type="email" 
+                 placeholder="Enter Your email" 
+                 name="email"
+                 onChange={inputEvent}
+                 value={fullName.email}
+                 autoComplete="off"
+                  />
+                  <input
+                 type="number" 
+                 placeholder="Enter Your Phone No" 
+                 name="phone"
+                 onChange={inputEvent}
+                 value={fullName.phone}
                   />
                 <button type="submit"> Submit Me 👍 </button>
             </div>
