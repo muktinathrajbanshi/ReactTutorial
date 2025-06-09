8b244d414251c0c3cb21531d0a927097a4957dd7
unknown <muktinathrajbanshi786@gmail.com>
2025-06-09 19:19:54 +0545

    NPM (Node Package Manager) Mini Project

--- src/ToDoLists.jsx
+++ src/ToDoLists.jsx
@@ -1,40 +1,9 @@
-import React, { useState } from "react";
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React from "react";
+import Clock from 'react-digital-clock';
+
 
 const ToDoLists = () => {
-
-  const [num, setNum] = useState(0);
-
-  const incNum = () => {
-    setNum(num+1);
-  }
-
-  const decNum = () => {
-    if(num > 0) {
-      setNum(num-1);
-    }else{
-      alert("Sorry, Zero Limit Reached")
-      setNum(0);
-    }
-  }
-
-  return(
-    <>
-      <div className="main_div">
-        <div className="center_div">
-          <h1> {num} </h1>
-          <div className="btn_div">
-            <button onClick={incNum}>
-            <AddIcon /> 
-            </button>
-            <button onClick={decNum}> 
-            <DeleteIcon />
-             </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+   return <Clock />
 };
+
 export default ToDoLists;
