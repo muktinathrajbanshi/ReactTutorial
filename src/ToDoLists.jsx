0dea051d75d1a207bdfc8dac7f29041b7c2258e2
unknown <muktinathrajbanshi786@gmail.com>
2025-06-10 13:01:23 +0545

    using material ui ToDo List App in React

--- src/ToDoLists.jsx
+++ src/ToDoLists.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-import Clock from 'react-digital-clock';
-
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 const ToDoLists = () => {
-   return <Clock />
+
+   return (
+      <>
+         <div className="main_div">
+            <div className="center_div">
+               <br />
+               <h1> To Do List </h1>
+               <br />
+               <input type="text" 
+               placeholder="Add an Items"
+                />
+                <Button className="newBtn">
+                  <AddIcon />
+                </Button>
+            </div>
+         </div>
+      </>
+   );
+
 };
 
 export default ToDoLists;
