a8f99252082098b280f70584d5948e3331a71a42
unknown <muktinathrajbanshi786@gmail.com>
2025-06-10 18:19:25 +0545

    React Bootstrap Autocomplete Extension in VS Code i.e IntelliSense for CSS class names in HTML

--- src/ToDoLists.jsx
+++ src/ToDoLists.jsx
@@ -4,26 +4,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const ToDoLists = () => {
    return(
       <>
-         <h1> Welcome to my Page </h1>
-         <button className="btn btn-success"> MuktinathTech </button>
-
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" />
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-         
+         <h1 className="text-capitalize text-center mt-5 text-danger"> Welcome to my Page </h1>
+                
       </>
    );
 };
