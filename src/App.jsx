7571f8d338467c861190bf986f3c527fde7c2725
unknown <muktinathrajbanshi786@gmail.com>
2025-06-12 02:33:29 +0545

    Context API in React JS like createContext, consumer, provider, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,20 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import CreateNote from "./CreateNote";
-import Note from "./Note";
+import React, { createContext } from "react";
+import ComA from "./ComA";
+
+const FirstName = createContext();
+const LastName = createContext();
 
 const App = () => {
-    const [addItem, setAddItem] = useState([]);
-
-    const addNote = (note) => {
-        // alert("i am clicked")
-        setAddItem((prevData) => {
-            return [...prevData, note];
-        });
-
-        console.log(note);
-    };
-
-    const onDelete = (id) => {
-        setAddItem((oldData) => 
-            oldData.filter((currData, indx) => {
-                return indx !== id;
-            })
-        );
-    };
-
-
-    return(
+    return (
         <>
-       <Header />
-       <CreateNote passNote = {addNote}/>
-
-        {addItem.map((val, index) => {
-            return <Note
-                key={index}
-                id={index}
-                title={val.title}
-                content={val.content}
-                deleteItem = {onDelete}
-             />
-        })}
-
-       <Footer />
+        <FirstName.Provider value={"Love"}>
+        <LastName.Provider value={"You"}> 
+        <ComA />   
+         </LastName.Provider>
+        </FirstName.Provider>
         </>
-    );
+    ); 
 };
 
 export default App;
+export {FirstName, LastName};
