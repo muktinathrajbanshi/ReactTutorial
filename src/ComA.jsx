f54d3ba68730dc2f105a4c10610bad4b106528ed
unknown <muktinathrajbanshi786@gmail.com>
2025-06-12 21:15:57 +0545

    one challenge is complete i.e fetching image from api and showing inside useEffect

--- src/ComA.jsx
+++ src/ComA.jsx
@@ -6,24 +6,25 @@ const ComA = () => {
     const [num, setNum] = useState();
     const [name, setName] = useState();
     const [moves, setMoves] = useState();
-
+    const [image, setImage] = useState("");
     useEffect(() => {
         // alert("hi");
         async function getData(){
             const res = await axios.get(` https://pokeapi.co/api/v2/pokemon/${num}`)
-            console.log(res.data);
             setName(res.data.name);
             setMoves(res.data.name.length);
+            setImage(res.data.sprites.front_default);
         }
         getData();
-    });
+    }, [num]);
 
     return (
         <>
             <h1> you choose <span style={{color:"red"}}> {num} value </span></h1>
             <h1> My name is <span style={{color:"red"}}> {name} </span></h1>
             <h1> I have <span style={{color:"red"}}> {moves} moves </span></h1>
-
+            {image && <img src={image} alt={name} style={{ width: "400px", height: "auto", marginLeft: "620px" }} />}
+            <br />
            <select value={num} onChange={(event) => {
             setNum(event.target.value);
            }}>
