5d6188d047df264127258b72f3a4bff4fd876870
unknown <muktinathrajbanshi786@gmail.com>
2025-06-12 20:45:12 +0545

    React API Call to Get Pokemon JSON Data using Axios and useEffect in ReactJS

--- src/ComA.jsx
+++ src/ComA.jsx
@@ -1,8 +1,40 @@
-import React from "react";
-import ComB from "./ComB";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ComA = () => {
-    return <ComB />;
+
+    const [num, setNum] = useState();
+    const [name, setName] = useState();
+    const [moves, setMoves] = useState();
+
+    useEffect(() => {
+        // alert("hi");
+        async function getData(){
+            const res = await axios.get(` https://pokeapi.co/api/v2/pokemon/${num}`)
+            console.log(res.data);
+            setName(res.data.name);
+            setMoves(res.data.name.length);
+        }
+        getData();
+    });
+
+    return (
+        <>
+            <h1> you choose <span style={{color:"red"}}> {num} value </span></h1>
+            <h1> My name is <span style={{color:"red"}}> {name} </span></h1>
+            <h1> I have <span style={{color:"red"}}> {moves} moves </span></h1>
+
+           <select value={num} onChange={(event) => {
+            setNum(event.target.value);
+           }}>
+            <option value="1">1</option>
+            <option value="25">25</option>
+            <option value="3">3</option>
+            <option value="4">4</option>  
+            <option value="5">5</option>
+           </select>
+        </>
+    );
 };
 
 export default ComA;
