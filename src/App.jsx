a06454aa510017c616ebbf65ad470c4d3b92925a
unknown <muktinathrajbanshi786@gmail.com>
2025-06-12 17:43:34 +0545

    React Hook Challenge 7 | Changing the Title value of Website on Button Click

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,15 @@ import React, { useEffect, useState } from "react";
 const App = () => {
 
     const [num, setNum] = useState(0);
-    const [nums, setNums] = useState(0);
+    useEffect(() =>{
+        document.title = `you clicked me ${num} times`
+    })
 
-
-    useEffect(() => {
-         alert("i am clicked");
-         console.log("i am clicked");
-    }, [num]);
-
-    return(
+    return (
         <>
-            
-            <button onClick={(() => {
-                setNum(num + 1);
-                })
-            }
-            > click me {num} </button>
-            <br />
-            <button onClick={(() => {
-                setNums(nums + 1);
-                })
-            }
-            > click me {nums} </button>
+            <button onClick={() => {
+                setNum(num + 1)
+            }}> click me {num} </button>
         </>
     );
 };
