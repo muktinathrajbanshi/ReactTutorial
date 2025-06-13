1304b2ef163bf8a8790cb26ba91e4b1b5d3ff97f
unknown <muktinathrajbanshi786@gmail.com>
2025-06-13 19:35:53 +0545

    React Navbar | MENU using React Router in ReactJS

---
+++ src/Menu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Menu = () => {
+    return(
+        <>
+        <NavLink exact activeClassName="active_class" to="/"> About Us </NavLink>
+        <NavLink activeClassName="active_class" to="/contact"> Contact Us </NavLink>
+        <br />
+          <a href="/"> About Us </a>
+          <a href="/contact"> Contact Us </a>
+
+        </>
+    );
+};
+
+export default Menu;
