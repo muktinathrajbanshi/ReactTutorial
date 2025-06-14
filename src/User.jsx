fa35d6c5a6cdeaa0b531e9028497ca167cc65f92
unknown <muktinathrajbanshi786@gmail.com>
2025-06-14 12:59:27 +0545

    useLocation Hook in React Router

--- src/User.jsx
+++ src/User.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const User = () => {
     const {name, lname} = useParams();
+    const location = useLocation();
+    console.log(location);
     return (
         <>
             <h1> user {name} {lname} page </h1>
+            <p> My current location is {location.pathname} </p>
+            {location.pathname === `/user/muktinath/rajbanshi` ? (
+            <button onClick={ () => alert(`you are awesome`) }> Click Me </button>) : null}
         </>
     );
 };
