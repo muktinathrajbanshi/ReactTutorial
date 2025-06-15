5b64e3f5a5b6c3b8bffc2d7928874d2822081424
unknown <muktinathrajbanshi786@gmail.com>
2025-06-15 17:23:12 +0545

    finally my new project is started by using bootstrap-5 and reactjs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render( 
-  
+  <>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  
+  </>,
   document.getElementById("root")
 );
