e43534608100a6887a7818973d75ac8f595507eb
unknown <muktinathrajbanshi786@gmail.com>
2025-06-15 22:30:32 +0545

    home section just started

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, activeClassName } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -17,16 +17,16 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                <NavLink activeClassName="menu_active" exact className="nav-link active" aria-current="page" to="/">Home</NavLink>
                 </li>
                 <li className="nav-item">
-                <NavLink className="nav-link" to="/service">Services</NavLink>
+                <NavLink activeClassName="menu_active" className="nav-link" to="/service">Services</NavLink>
                 </li>
                 <li className="nav-item">
-                <NavLink className="nav-link" to="/about">About</NavLink>
+                <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
                 </li>
                 <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                <NavLink activeClassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
                 </li>
             </ul>
             
