2740bdca2bc76da742d460228d4697e8566d95f6
unknown <muktinathrajbanshi786@gmail.com>
2025-06-16 00:33:31 +0545

    creating API for card purpose dynamically for service page

---
+++ src/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import web from "../src/Images/about.png"
+import { NavLink } from "react-router-dom";
+
+const Card = () => {
+    return(
+        <>
+        <div className="container-fluid mb-5">
+            <div className="row">
+              <div className="col-10 mx-auto">
+                    <div className="col-md-4 col-10 mx-auto">
+                        <div class="card">
+                        <img src={web} class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <NavLink to="/" class="btn btn-primary">Go somewhere</NavLink>
+                        </div>
+                        </div>
+                    </div>
+             </div>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Card;
---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+import web from "../src/Images/web.webp";
+import app from "../src/Images/app.png";
+import android from "../src/Images/android.webp";
+import digital from "../src/Images/digital.jpg";
+import marketing from "../src/Images/marketing.jpeg";
+import software from "../src/Images/software.webp";
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "Web development"
+    },
+     {
+        imgsrc: app,
+        title: "App development"
+    },
+     {
+        imgsrc: software,
+        title: "Software development"
+    }, {
+        imgsrc: digital,
+        title: "Digital Marketing"
+    },
+     {
+        imgsrc: android,
+        title: "Android development"
+    },
+     {
+        imgsrc: marketing,
+        title: "Marketing"
+    }, 
+];
+
+
