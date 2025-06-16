beec13f99964d1f37fdf3162b559a855804f7392
unknown <muktinathrajbanshi786@gmail.com>
2025-06-16 18:56:37 +0545

    for toggling navbar i have to change bootstrap version

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
     return(
         <>
-            <footer className="bg-light text-center">
+            <footer className="bg-light d-flex justify-content-center align-items-center pt-2 mb-0 fixed-bottom" style={{height:"40px", lineHeight:"45px"}}>
                 <p> © 2020 MuktinathTech. All Rights Reserved | Terms and Condition </p>
             </footer>
         </>
