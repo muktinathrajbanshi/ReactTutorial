f7c403f0372de03981cf9de9e45a5e74b0bb8d07
unknown <muktinathrajbanshi786@gmail.com>
2025-06-17 12:57:49 +0545

    just installing SASS in React Project

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 
 const Footer = () => {
+
+    const year = new Date().getFullYear();
+
     return(
         <>
             <footer className="bg-light d-flex justify-content-center align-items-center pt-2 mb-0 fixed-bottom" style={{height:"40px", lineHeight:"45px"}}>
-                <p> © 2020 MuktinathTech. All Rights Reserved | Terms and Condition </p>
+                <p> © {year} MuktinathTech. All Rights Reserved</p>
             </footer>
         </>
     );
